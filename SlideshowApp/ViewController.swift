429e8a378061fe7855ecee7f1e5b5d33ce8bee7f
takuma.arimura <aritaku@ezweb.ne.jp>
2020-08-17 17:14:29 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 有村卓馬 on 2020/08/17.
+//  Copyright © 2020 takuma.arimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
